c9d727177af3c345e783f26bf038822355972177
Reid Chatham <reid.chatham@gmail.com>
2017-03-23 15:16:33 -0700

    update ARAnnotationView

--- HDAugmentedReality/Classes/ARAnnotationView.swift
+++ HDAugmentedReality/Classes/ARAnnotationView.swift
@@ -10,37 +10,20 @@ import UIKit
 
 /// View for annotation. Subclass to customize. Annotation views should be lightweight,
 /// try to avoid xibs and autolayout.
-/// bindUi method is called when distance/azimuth is set in ARViewController.
 open class ARAnnotationView: UIView {
     open weak var annotation: ARAnnotation?
-    fileprivate var initialized: Bool = false
     
     public init() {
         super.init(frame: CGRect.zero)
-        initializeInternal()
     }
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        initializeInternal()
     }
     
-    override init(frame: CGRect) {
+    override public init(frame: CGRect) {
         super.init(frame: frame)
-        initializeInternal()
     }
     
-    fileprivate func initializeInternal() {
-        if initialized { return }
-        initialized = true
-        initialize()
-    }
     
-    open override func awakeFromNib() { bindUI() }
-    
-    /// Will always be called once, no need to call super
-    open func initialize() {}
-    
-    /// Called when distance/azimuth changes, intended to be used in subclasses
-    open func bindUI() {}
 }
